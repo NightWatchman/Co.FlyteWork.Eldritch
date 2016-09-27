fb3090d53e0160ca0270f5b27586348d94282c2f
Eric Rushing <rushinge@gmail.com>
2016-09-27 13:48:44 -0600

    Configure babel with ES2016 loader

--- webpack.config.js
+++ webpack.config.js
@@ -3,5 +3,15 @@ module.exports = {
   output: {
     path: __dirname + '/scripts/built',
     filename: 'app.js'
-  }
+  },
+  loaders: [
+    {
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: "babel-loader",
+      query: {
+        presets: ['es2015']
+      }
+    }
+  ]
 }
